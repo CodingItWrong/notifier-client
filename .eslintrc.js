56160f48574950628f212b395e74e072921ba069
Josh Justice <me@codingitwrong.com>
2020-08-11 13:50:04 -0400

    Add import rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,15 @@
 module.exports = {
   root: true,
   extends: '@react-native-community',
+  plugins: ['import'],
   rules: {
+    'import/newline-after-import': 'error',
+    'import/no-anonymous-default-export': 'error',
+    'import/no-mutable-exports': 'error',
+    'import/order': ['error', { alphabetize: { order: 'asc' } }],
+    'no-duplicate-imports': 'error',
     'react-native/no-inline-styles': 'off',
+    'sort-imports': ['error', { ignoreDeclarationSort: true }],
   },
   globals: {
     WebSocket: true,
